4b03355b24113ef31d38580ab10788fe0304297f
xavier chia <xavier.cpy@gmail.com>
2023-11-16 07:03:54 +0800

    feat: Add countdown timer UI.

--- runwithfriends/runwithfriends/Runs/WaitingRoomViewController/WaitingRoomViewController.swift
+++ runwithfriends/runwithfriends/Runs/WaitingRoomViewController/WaitingRoomViewController.swift
@@ -14,6 +14,7 @@ class WaitingRoomViewController: UIViewController {
     let mapView = MKMapView()
     let bottomRow: BottomRow
     let locationManager = CLLocationManager()
+    var pinsSet = false
     
     // coordinates for the The Panathenaic Stadium, where the first Olympic games were held
     let defaultLocation = CLLocationCoordinate2D(latitude: 37.969, longitude: 23.741)
@@ -29,10 +30,11 @@ class WaitingRoomViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
         setupLocationManager()
         setupBottomRow()
         setupMapView()
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        setupCountdownTimer()
     }
     
     private func setupLocationManager() {
@@ -81,6 +83,8 @@ class WaitingRoomViewController: UIViewController {
     }
     
     private func setMapRegion(with coordinate: CLLocationCoordinate2D) {
+        pinsSet = true
+        
         let obscuredCoordinate = coordinate.obscured()
         // Handle location update
         let span = MKCoordinateSpan(latitudeDelta: 50, longitudeDelta: 50)
@@ -112,6 +116,24 @@ class WaitingRoomViewController: UIViewController {
         philPin.title = "Kurt"
         mapView.addAnnotation(philPin)
     }
+    
+    private func setupCountdownTimer() {
+        let countdownTimer = UILabel()
+        countdownTimer.text = "Countdown: 5:00"
+        countdownTimer.font = UIFont.systemFont(ofSize: countdownTimer.font.pointSize, weight: .bold)
+        countdownTimer.textAlignment = .center
+        countdownTimer.backgroundColor = .black
+        countdownTimer.layer.cornerRadius = 10
+        countdownTimer.layer.masksToBounds = true
+        view.addSubview(countdownTimer)
+        countdownTimer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            countdownTimer.widthAnchor.constraint(equalToConstant: 170),
+            countdownTimer.heightAnchor.constraint(equalToConstant: 40),
+            countdownTimer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            countdownTimer.bottomAnchor.constraint(equalTo: bottomRow.topAnchor, constant: -20)
+        ])
+    }
 }
 
 // MARK: CLLocationManagerDelegate
@@ -132,11 +154,18 @@ extension WaitingRoomViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("Getting location passed")
+        if pinsSet == false,
+           let location = manager.location {
+            print("Getting location passed")
+            setMapRegion(with: location.coordinate)
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Getting location failed")
+        if pinsSet == false {
+            print("Getting location failed")
+            setMapRegion(with: defaultLocation)
+        }
     }
 }
 
