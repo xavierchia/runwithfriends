637df3ed9ed75e67648a22f94074694f8fb14800
xavierchia <xavier.cpy@gmail.com>
2025-03-19 08:45:03 +0800

    fix saving friends as users in main app, bump build to 43.

--- runwithfriends/runwithfriends/Community/CommunityViewController.swift
+++ runwithfriends/runwithfriends/Community/CommunityViewController.swift
@@ -97,11 +97,7 @@ class CommunityViewController: UIViewController {
             userData.updateStepsIfNeeded(dailySteps: result)
             let pubs = await userData.getPublicUsers()
             
-            let friendsProgress = pubs.map { user in
-                return FriendProgress(user_id: user.user_id, username: user.username, steps: user.day_steps ?? 0)
-            }
-            
-            FriendsManager.shared.updateFriends(friendsProgress)
+            FriendsManager.shared.updateFriends(pubs)
             
             await MainActor.run {
                 mapView.removeAnnotations(mapView.annotations)
--- runwithfriends/runwithfriends/Community/FriendsManager.swift
+++ runwithfriends/runwithfriends/Community/FriendsManager.swift
@@ -8,12 +8,6 @@
 import Foundation
 import WidgetKit
 
-struct FriendProgress: Codable {
-    let user_id: UUID
-    let username: String
-    let steps: Int
-}
-
 class FriendsManager {
     static let shared = FriendsManager()
     private let defaults = UserDefaults(suiteName: "group.com.wholesomeapps.runwithfriends")
@@ -21,7 +15,7 @@ class FriendsManager {
     
     private init() {}
     
-    func updateFriends(_ friends: [FriendProgress]) {
+    func updateFriends(_ friends: [User]) {
         guard let defaults = defaults else { return }
         do {
             print("saved friends")
@@ -32,21 +26,5 @@ class FriendsManager {
         } catch {
             print("Failed to save friends data: \(error)")
         }
-    }
-    
-    func getFriends() -> [FriendProgress] {
-        guard let defaults = defaults else { return [] }
-        
-        if let data = defaults.data(forKey: friendsKey) {
-            do {
-                let friends = try JSONDecoder().decode([FriendProgress].self, from: data)
-                return friends
-            } catch {
-                print("Failed to decode friends data: \(error)")
-                return []
-            }
-        }
-        
-        return []
     }
 }
